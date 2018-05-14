56ef01db30dba6cfe5305cf96c664f09c43c853f
RobertGJr <39225262+RobertGJr@users.noreply.github.com>
2018-05-14 02:45:21 -0400

    Update script.js

--- StartUp/script.js
+++ StartUp/script.js
@@ -1,6 +1,7 @@
 var today = document.getElementById('day');
 var todayMin = document.getElementById('minutes');
 var todayHour = document.getElementById('hours');
+var study = document.getElementById('study');
 
 var hour = new Date();
 var min = new Date();
@@ -30,3 +31,18 @@ switch (new Date().getDay()) {
 today.textContent = "today is" + day;
 todayHour.textContent = hour.getHours() + ":";
 todayMin.textContent = min.getMinutes();
+if (day === "Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday") {
+    if (hour < 12 && minute < 59) {
+        study.textContent = "Sir, Classes start at 13:00 pm, try no to be late."
+    } else {
+        study.textContent = "...";
+    }
+} else if (day === "Friday") {
+    if (hour < 11 && minute < 59) {
+    study.textContent = "Sir, Classes start at 12:00 pm, try no to be late."
+    } else {
+    study.textContent = "...";
+    }
+} else {
+    study.textContent = "Sir, there are no classes today."
+}
